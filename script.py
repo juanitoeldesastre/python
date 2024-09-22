041045203ee21dc7ddfa7657f8e1843c838f9290
juanito <juanitoeldevastador@gmail.com>
2024-09-22 04:20:42 +0000

    1

---
+++ script.py
@@ -0,0 +1,20 @@
+print("Hola mundo")
+print( 1 + 1)
+nombre = "juan"
+print(f"Hola soy {nombre} y tengo", 10 + 10, "años" )
+amigos = ["mariana","karolg","scrappy"]
+print(amigos[2])
+scooby = amigos[0]
+dobidu = amigos[1]
+print(scooby,dobidu)
+print(f"Mi vida en otra realidad alterna es con {scooby} y en mi otra vida es con {dobidu}")
+
+
+tupla = ("piedra", "caca", "chisito")
+print(tupla)
+
+poke = input("Ingrese un pokemon para el laboratorio del Profesor Ouk :")  
+lista=["charmander","bulbasaur","squirtle"]
+lista.append(poke)
+print(f"Profesor Ouk recibe un {poke} de regalo")
+print("Profesor Ouk tiene estos pokemons", lista)
