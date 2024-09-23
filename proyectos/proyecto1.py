766f4e52e13fb0a4207993357b70d135bebeb6e7
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-09-22 19:39:36 -0500

    add to ValueError

--- proyectos/proyecto1.py
+++ proyectos/proyecto1.py
@@ -2,15 +2,19 @@
 #reglas : no puedo usar chatgpt
 
 import random
+
 def numero():
-    aleatorio = random.randrange(1,101)
+    aleatorio = random.randrange(1,10)
     return aleatorio
 numero_aleatorio = numero()
 
-adivinanza = int(input("Adivine el numero que piensa la maquina:"))
-if adivinanza==numero_aleatorio:
+try:
+    adivinanza = int(input("Adivine el numero que piensa la maquina:"))
+    if adivinanza==numero_aleatorio:
      print(f"Adivinaste el numero {numero_aleatorio}")
-else:
-     print(f"Vales madre el numero era {numero_aleatorio}") 
+    else:
+     print(f"Vales madre el numero era {numero_aleatorio}")
+except ValueError:
+     print("Ingrese un numero porfavor")
 
 
